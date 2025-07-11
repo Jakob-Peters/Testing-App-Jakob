c6535bf84db7037b73281bb91d4c9800d4299290
Jakob Peters <jaspe@stepentwork.dk>
2025-07-11 11:23:04 +0200

    Initial Commit

---
+++ Testing App Jakob/Testing_App_JakobApp.swift
@@ -0,0 +1,17 @@
+//
+//  Testing_App_JakobApp.swift
+//  Testing App Jakob
+//
+//  Created by Jakob Svanborg Peters on 11/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct Testing_App_JakobApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
