b260ee3cc992ba540dfda3f942bf886614822d0d
Jakob Peters <jaspe@stepentwork.dk>
2025-07-11 12:46:33 +0200

    setup

---
+++ Features/consentManagement/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  Testing App Jakob
+//
+//  Created by Jakob Svanborg Peters on 11/07/2025.
+//
+
+import Foundation
---
+++ Testing App Jakob/AdWebView.swift
@@ -0,0 +1,8 @@
+//
+//  AdWebView.swift
+//  Testing App Jakob
+//
+//  Created by Jakob Svanborg Peters on 11/07/2025.
+//
+
+import Foundation
